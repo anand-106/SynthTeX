d253cfce728f97d24183fabe6571daec2a2dd0a6
anand-106 <anands2003106@gmail.com>
2026-01-08 21:42:54 +0530

    added docker compose

--- apps/compiler/worker.py
+++ apps/compiler/worker.py
@@ -1,9 +1,12 @@
+import os
 from arq.connections import RedisSettings
 
 from tasks import latex_job_compiler
 
+redis_dsn = os.getenv("REDIS_URL", "redis://synthtex-redis:6379")
 
-REDIS_SETTINGS = RedisSettings.from_dsn("redis://localhost:6379")
+
+REDIS_SETTINGS = RedisSettings.from_dsn(redis_dsn)
 
 class WorkerSettings:
     functions= [latex_job_compiler]
