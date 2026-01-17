d40d1c02900a0741a226845260386692e2223717
anand-106 <anands2003106@gmail.com>
2026-01-17 11:15:06 +0530

    changed cors

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -12,7 +12,7 @@ app.include_router(chat_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://synthtex.anands.codes","https://www.synthtex.anands.codes"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
